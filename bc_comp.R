7221b2f57d71d8281bc06ee972a2e48f80bfbe56
Valentina <valybionda@hotmail.com>
2021-02-20 17:28:56 +0000

    Update bc_comp.R

--- bc_comp.R
+++ bc_comp.R
@@ -1,3 +1,15 @@
+#' Calculates Percentage of core compression and Linear Correction Factor
+#'
+#' This function calculates the Percentage of core compression and the Linear Correction Factor using three arguments.
+#' @param tube_lenght The lenght in cm of the sampler.
+#' @param core_in The lenght in cm of the part of the sampler left outside of the sediment (from the inside of the sampler).
+#' @param core_out The lenght in cm of the part of the sampler left outside of the sediment (from the outside of the sampler).
+#' @return Percentage of core compression and the Linear Correction Factor.
+#' @export
+#' @examples
+#' bc_comp(200, 48, 25)
+
+
 bc_comp <-
   function(tube_lenght, core_in, core_out) {
     
@@ -7,6 +19,8 @@ bc_comp <-
     ## compute correction factor 
     corr <- (tube_lenght - core_in) / (tube_lenght - core_out)
     
-    list("Percentage of core compression" = compr,
-         "Correction factor" = corr)
+    output <- list("Percentage of core compression" = compr,
+                   "Linear Correction factor" = corr)
+    
+    return(output) 
   }
