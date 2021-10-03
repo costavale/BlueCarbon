11e271cce63d48387be73939025684ce5a7418d8
MarcioFCMartins <marciomartinsred@gmail.com>
2021-10-03 09:44:54 +0100

    Implemented stock diagnostic plot for rectangle method
    Improved the way how core sections are extrapolated using the rectangle method
    to make visualization of the diagnostic plots easier
    The "rectangle" stock estimation method now has a diagnostic visualization

--- R/bc_stocks.R
+++ R/bc_stocks.R
@@ -1,9 +1,9 @@
 #' estimate_stock()
 #'
-#' Estimate the stock of an element contained given its concentration along 
+#' Estimate the stock of an element contained given its concentration along
 #' a sediment core
-#' 
-#' @param sample_data 
+#'
+#' @param sample_data
 #'
 #'
 #' @export
@@ -12,34 +12,32 @@
 # TODO: Add data checks when user provides section heights: is the final result
 #   equal to maximum_depth?
 
-bc_stocks <- function(
-  sample_data,
-  core_id,
-  sample_depth,
-  element_concentration,
-  maximum_depth,
-  method = NULL,
-  section_height = NULL,
-  diagnostic_plot = FALSE
-){
+bc_stocks <- function(sample_data,
+                      core_id,
+                      sample_depth,
+                      element_concentration,
+                      maximum_depth,
+                      method = NULL,
+                      section_height = NULL,
+                      diagnostic_plot = FALSE) {
   
-  if(is.null(method) | !method %in% c("rectangle", "trapezoid")){
+  if (is.null(method) | !method %in% c("rectangle", "trapezoid")) {
     stop("Please select a stock estimation method.\n
          Consult documentation for method descriptions.")
   }
-  
-  if(method == "trapezoid"){
+
+  if (method == "trapezoid") {
     sample_data <- split(
-      sample_data, 
+      sample_data,
       as.factor(sample_data$core_id)
     )
-    
+
     stocks <- sapply(
       sample_data,
       FUN = function(x) {
         stock <- MESS::auc(
-          x = x[ , sample_depth],
-          y = x[ , element_concentration],
+          x = x[, sample_depth],
+          y = x[, element_concentration],
           from = 0,
           to = maximum_depth,
           type = "linear",
@@ -47,90 +45,118 @@ bc_stocks <- function(
         )
       }
     )
-    
+
     stocks <- data.frame(
       "core_id" = names(stocks),
-      "element_stock" = stocks)
+      "element_stock" = stocks
+    )
+
+    if (diagnostic_plot) plot_stock_diagnostic(sample_data, method)
     
-  } else if(method == "rectangular"){
+    return(stocks)
+
+  } else if (method == "rectangle") {
     # Height of sections represented by each slice are given
-    if(!is.null(section_height)){
-      sample_data$section_start <- sample_data[ , sample_depth] - 
-                                   sample_data[, section_height]/2
-      
-      sample_data$section_end <- sample_data[ , sample_depth] + 
-                                 sample_data[, section_height]/2
-      
+    if (!is.null(section_height)) {
+      stop("Method is broken")
+      # Calculate start and end of core sections
+      sample_data$section_start <- sample_data[, sample_depth] -
+        sample_data[, section_height] / 2
+
+      sample_data$section_end <- sample_data[, sample_depth] +
+        sample_data[, section_height] / 2
+
       sample_data <- split(sample_data, f = sample_data[, core_id])
-      
+
       # Correct data so that stocks are estimated for `maximum_depth`
       sample_data <- sapply(
         sample_data,
-        FUN = function(x){
-          x <- x[order(x[ , sample_depth]), ]
-          # Remove sections deeper than needed
-          x <- x[x$section_start < maximum_depth , ]
-          # If last section goes over `maximum depth`, shorten it
-          x$section_end <- if(x$section_end > maximum_depth){
-            x$section_end - (x$section_end - maximum_depth)
-            }
-          # If last section doesn't reach `maximum depth`, extend it
-          x$section_end <- if(x$section_end < maximum_depth){
-            x$section_end + (maximum_depth - x$section_end)
+        FUN = function(x) {
+          x <- x[order(x[, sample_depth]), ]
+
+          # Identify if last layer needs to be extrapolated
+          x$extrapolated <- FALSE
+          x$extrapolated[nrow(x)] <- x[nrow(x), sample_depth] < maximum_depth
+
+          # Remove samples deeper than needed
+          x <- x[x$section_start < maximum_depth, ]
+
+          # Create new section that extrapolates the elemental profile
+          if (any(x$extrapolated)) {
+            # Add new section
+            x <- rbind(x, x[nrow(x), ])
+            # Only the actually extrapolated section is flagged as extrapolated
+            x$extrapolated[1:(nrow(x) - 1)] <- FALSE
+            # Extrapolated layer starts at end of previous one
+            x[nrow(x), "section_start"] <- x[nrow(x), "section_end"]
+            # Extrapolated layer ends at `maximum_depth`
+            x[nrow(x), "section_end"] <- maximum_depth
           }
         }
       )
-      
-      # Calculate new section heights
-      sample_data[ , section_height] <- sample_data$section_end - 
-                                        sample_data$section_start
-      
-      # Calculate carbon stock per section
-      sample_data$section_stock <- sample_data[ , element_concentration] *
-                                   sample_data[ , section_height]
-      
-      stocks <- aggregate(
-        formula = c(section_stock) ~ core_id,
-        data = sample_data,
-        FUN = function(x) c(stock = sum(x))
-      )
-    # If height of sections represented by each slice are not provided
+      # If height of sections represented by each slice are not given
     } else {
       sample_data <- split(sample_data, f = sample_data[, core_id])
-      
+
+      # Calculate core section heights and extrapolate
       sample_data <- lapply(
         sample_data,
-        FUN = function(x){
-          x <- x[order(x[ , sample_depth]), ]
+        FUN = function(x) {
+          x <- x[order(x[, sample_depth]), ]
           # Estimate the middle point from current sample to next and previous
           # sample - this is determined as the section represented by the sample
-          x$height_below <- (c(x[-1 ,sample_depth], NA)  - x[ ,sample_depth])/2
-          x$height_above <- x[ , sample_depth] - c(0, x[-nrow(x), sample_depth])
-          x$height_above[-1] <- x$height_above[-1]/2
-          x$section_start <- x[ , sample_depth] - x$height_above
-          x$section_end <- x[ , sample_depth] + x$height_below
+          x$height_below <- (c(x[-1, sample_depth], NA) - x[, sample_depth]) / 2
+          x$height_above <- x[, sample_depth] - c(0, x[-nrow(x), sample_depth])
+          x$height_above[-1] <- x$height_above[-1] / 2
+          x$section_start <- x[, sample_depth] - x$height_above
+          x$section_end <- x[, sample_depth] + x$height_below
+
+          # Identify if last layer needs to be extrapolated
+          x$extrapolated <- FALSE
+          x$extrapolated[nrow(x)] <- x[nrow(x), sample_depth] < maximum_depth
+
           # Remove samples deeper than needed
           x <- x[x$section_start < maximum_depth, ]
-          # Correct end of last section be be equal to max stock depth
-          x$section_end[nrow(x)] <- maximum_depth
-          x$section_height <- x$section_end - x$section_start
-          
+
+          # Create new section that extrapolates the elemental profile
+          if (any(x$extrapolated)) {
+            # Set end of last recorded section to measured sample depth
+            x[nrow(x), "section_end"] <- x[nrow(x), sample_depth]
+            # Add new section
+            x <- rbind(x, x[nrow(x), ])
+            # Only the actually extrapolated section is flagged as extrapolated
+            x$extrapolated[1:(nrow(x) - 1)] <- FALSE
+            # Extrapolated layer starts at end of previous one
+            x[nrow(x), "section_start"] <- x[nrow(x), "section_end"]
+            # Extrapolated layer ends at `maximum_depth`
+            x[nrow(x), "section_end"] <- maximum_depth
+          }
+
           return(x)
         }
       )
-        
-        sample_data <- do.call(rbind, sample_data)
-        
-        # Calculate carbon stock per section
-        sample_data$section_stock <- sample_data[ , element_concentration] *
-          sample_data$section_height
-        
-        stocks <- aggregate(
-          formula = c(section_stock) ~ core_id,
-          data = sample_data,
-          FUN = function(x) c(stock = sum(x))
-        )
     }
+
+    if(diagnostic_plot) plot_stock_diagnostic(sample_data, method)
+
+    sample_data <- do.call(rbind, sample_data)
+
+    # Calculate new section heights
+    if(is.null(section_height)) section_height <- "section_height"
+    sample_data[, section_height] <- sample_data$section_end -
+      sample_data$section_start
+
+    # Calculate carbon stock per section
+    sample_data$section_stock <- sample_data[, element_concentration] *
+      sample_data[, section_height]
+
+    stocks <- aggregate(
+      formula = section_stock ~ core_id,
+      data = sample_data,
+      FUN = function(x) c(stock = sum(x))
+    )
+    
+    return(stocks)
   } else {
     message("Invalid method selected.")
   }
--- R/plot_stock_diagnostic.R
+++ R/plot_stock_diagnostic.R
@@ -1,31 +1,32 @@
 #' plot_stock_diagnostic
 #'
-#' Estimate the stock of an element contained given its concentration along
-#' a sediment core
+#' Internal function used to plot stock estimations in `bc_stocks`
 #'
-#' @param sample_data
-#'
-#'
+#' @param sample_data Sample data as a list, split by sediment core
+#' @param method Method used to calculate elemental stock
 
-plot_stock_diagnostic <- function() {
+plot_stock_diagnostic <- function(sample_data, method) {
+
+  # Add depth zero value
+  stock_points <- lapply(
+    sample_data,
+    FUN = function(x) {
+      x <- rbind(x, x[1, ])
+      x[nrow(x), sample_depth] <- 0
+      x <- x[order(x[, sample_depth]), ]
+      return(x)
+    }
+  )
+
+
+  # Trapezoid method visualization ------------------------------------------
   if (method == "trapezoid") {
-    sample_data <- lapply(
-      sample_data,
-      FUN = function(x) {
-        # Add a new row for depth zero
-        x <- rbind(x, x[1, ])
-        x[nrow(x), sample_depth] <- 0
-        x <- x[order(x[, sample_depth]), ]
-        return(x)
-      }
-    )
 
+    # If extrapolation is required, create data.frame to visualize it
     extrapolated_values <- lapply(
       sample_data,
       FUN = function(x) {
         need_extrapolation <- x[nrow(x), sample_depth] < maximum_depth
-
-        # If extrapolation is required, create data.frame to visualize it
         if (need_extrapolation) {
           extrapolated <- rbind(x[nrow(x), ], x[nrow(x), ])
           extrapolated[nrow(extrapolated), sample_depth] <- maximum_depth
@@ -34,26 +35,29 @@ plot_stock_diagnostic <- function() {
       }
     )
 
-    sample_data <- do.call(rbind, sample_data)
+    stock_points <- do.call(rbind, stock_points)
     extrapolated_values <- do.call(rbind, extrapolated_values)
+    sample_data <- do.call(rbind, sample_data)
 
-    ggplot(sample_data) +
-      geom_line(
+    stock_plot <- ggplot() +
+      geom_area(
+        data = stock_points,
         aes(
           x = .data[[sample_depth]],
           y = .data[[element_concentration]]
         )
       ) +
       geom_point(
+        data = sample_data,
         aes(
           x = .data[[sample_depth]],
           y = .data[[element_concentration]]
         )
       ) +
       facet_wrap(c(core_id)) +
-      scale_x_reverse(expand = expansion(c(0, 0.05))) +
+      scale_x_reverse(expand = expansion(c(0, 0.02))) +
       coord_flip(xlim = c(maximum_depth, 0)) +
-      if (need_extrapolation) {
+      if (!is.null(extrapolated_values)) {
         geom_line(
           data = extrapolated_values,
           aes(
@@ -64,11 +68,65 @@ plot_stock_diagnostic <- function() {
           linetype = "dashed"
         )
       }
-  } else if(method == "rectangle"){
-    if(!is.null(section_height)){
-      print("Implement section plots when user gives section heigts")
-    } else {
-      print("Implement section plots when calculated by function")
-    }
+
+    print(stock_plot)
+
+    # Rectangle method visualization ------------------------------------------
+  } else if (method == "rectangle") {
+
+    # If extrapolation is required, create data.frame to visualize it
+    extrapolated_values <- lapply(
+      sample_data,
+      FUN = function(x) {
+        x <- x[x$extrapolated, ]
+        if (!is.null(x)) {
+          extrapolated <- rbind(x[nrow(x), ], x[nrow(x), ])
+          extrapolated[nrow(extrapolated), "section_end"] <- maximum_depth
+          return(extrapolated)
+        }
+      }
+    )
+
+    stock_points <- do.call(rbind, stock_points)
+    extrapolated_values <- do.call(rbind, extrapolated_values)
+    sample_data <- do.call(rbind, sample_data)
+
+    stock_plot <- ggplot() +
+      geom_rect(
+        data = stock_points,
+        aes(
+          xmin = section_start,
+          xmax = section_end,
+          ymin = 0,
+          ymax = .data[[element_concentration]]
+        )
+      ) +
+      {
+        if (!is.null(extrapolated_values)) {
+          geom_rect(
+            data = extrapolated_values,
+            aes(
+              xmin = section_start,
+              xmax = section_end,
+              ymin = 0,
+              ymax = .data[[element_concentration]]
+            ),
+            color = "red",
+            linetype = "dashed"
+          )
+        }
+      } +
+      geom_point(
+        data = sample_data,
+        aes(
+          x = .data[[sample_depth]],
+          y = .data[[element_concentration]]
+        )
+      ) +
+      facet_wrap(c(core_id)) +
+      scale_x_reverse(expand = expansion(c(0, 0.05))) +
+      coord_flip(xlim = c(maximum_depth, 0))
+    
+    print(stock_plot)
   }
 }
