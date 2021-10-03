3a526ab1766bff484e816b7ae1eaf4d556b6468a
valybionda <vale.costa@gmail.com>
2021-10-03 11:18:14 +0200

    reformat lines 11-19

--- R/bc_stocks.R
+++ R/bc_stocks.R
@@ -1,34 +1,33 @@
 #' estimate_stock()
 #'
-#' Estimate the stock of an element contained given its concentration along 
+#' Estimate the stock of an element contained given its concentration along
 #' a sediment core
-#' 
-#' @param sample_data 
+#'
+#' @param sample_data
 #'
 #'
 #' @export
 
-bc_stocks <- function(
-  sample_data,
-  core_id,
-  sample_depth,
-  element_concentration,
-  maximum_depth,
-  method = NULL,
-  extrapolation_rule
-){
-  
+bc_stocks <-
+  function(sample_data,
+           core_id,
+           sample_depth,
+           element_concentration,
+           maximum_depth,
+           method = NULL,
+           extrapolation_rule) {
+
   if(is.null(method)){
     stop("Please select a stock estimation method.\n
          Consult documentation for method descriptions.")
   }
-  
+
   if(method == "trapezoid"){
     sample_data <- split(
-      sample_data, 
+      sample_data,
       as.factor(sample_data$core_id)
     )
-    
+
     stocks <- sapply(
       sample_data,
       FUN = function(x) {
@@ -42,20 +41,20 @@ bc_stocks <- function(
         )
       }
     )
-    
+
     stocks <- as.data.frame(
       "core_id" = names(stocks),
       "element_stock" = stocks)
-    
+
   } else if(method == "rectangular"){
     # TODO
     # Should the user give us the depth of the slice represented by the sample
-    # or should we calculate it? 
+    # or should we calculate it?
     # See thesis code to determine representative
     # depth of non-regularly distributed samples
     print("Method being implemented. Some assumptions must be verified")
   } else {
     stop("Invalid method selected.")
   }
-  
+
 }
